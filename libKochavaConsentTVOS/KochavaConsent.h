ac86e5a6f278984308d3ead5a6df131d00f6ce43
John Bushnell <jbushnell@kochava.com>
2021-02-18 14:19:37 -0800

    4.0.0

---
+++ libKochavaConsentTVOS/KochavaConsent.h
@@ -0,0 +1,32 @@
+//
+//  KochavaConsent.h
+//  KochavaConsent
+//
+//  Created by John Bushnell on 12/11/19.
+//  Copyright © 2020 Kochava, Inc.  All rights reserved.
+//
+
+
+
+#ifndef KochavaConsent_h
+#define KochavaConsent_h
+
+
+
+#import "KochavaCore.h"
+#import "KochavaUI.h"
+
+#import "KVAConsentAuditEntry.h"
+#import "KVAConsentClient.h"
+#import "KVAConsentConfiguration.h"
+#import "KVAConsentDialogNavigationController.h"
+#import "KVAConsentProduct.h"
+#import "KVAUSPrivacy.h"
+#import "KVAUSPrivacyStringValue.h"
+
+
+
+#endif
+
+
+
